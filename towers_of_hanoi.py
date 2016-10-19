c2e4fa0826911818bb38cb8f1b8213478487c8f4
John Leszczynski <jrleszczynski@gmail.com>
2016-10-18 20:12:58 -0400

    print letters if less than 26

--- towers_of_hanoi.py
+++ towers_of_hanoi.py
@@ -1,8 +1,12 @@
 class TowersOfHanoi(object):
 
     def __init__(self, num_disks):
+        letters = list('abcdefghijklmnopqrstuvwxyz'.upper())
         self.towers = [[] for _ in range(3)]
-        self.towers[0] = range(num_disks, 0, -1)
+        if num_disks <= len(letters):
+            self.towers[0] = letters[:num_disks][::-1]
+        else:
+            self.towers[0] = range(num_disks, 0, -1)
         self.move(num_disks, atPosition=0, left=True)
         print(self.tower_string)
 
@@ -15,8 +19,8 @@ class TowersOfHanoi(object):
 
         self.move(num_disks - 1, atPosition, not left)
 
-        print('{} ({} {})'.format(
-            self.tower_string, num_disks, 'left' if left else 'right')
+        print('{} {} {}'.format(
+            self.tower_string, self.towers[atPosition][-1], 'left' if left else 'right')
         )
         self.towers[target_position].append(self.towers[atPosition].pop())
 
